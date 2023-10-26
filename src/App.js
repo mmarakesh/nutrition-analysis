3062c92053c5e006578021cf14302a95507d957d
mmarakesh <marinamakarova749@gmail.com>
2023-10-26 12:53:15 +0200

    create a button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import NutritionComponent from './NutritionComponent';
 
 function App() {
+  const MY_ID = "996b8d70";
+  const MY_KEY = "13511e53a8922ecf13133c4451c2be24";
+
+  const [mySearch, setMySearch] = useState('');
+  const [myNutrition, setMyNutrition] = useState([]);
+
+  useEffect(() => {
+    getAnalysis();
+  }, [])
+
+  const getAnalysis = async () => {
+    const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${MY_ID}&app_key=${MY_KEY}&nutrition-type=logging&ingr=avocado`)
+    const data = await response.json();
+    console.log(data)
+    setMyNutrition(data);
+  }
+
+  const getData = (e) => {
+  console.log(e.target.value);
+  setMySearch(e.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nutrinion Analysis</h1>
+      <form>
+      <input onChange={getData} value={mySearch}></input>
+      </form>
+
+      <button>Click here</button>
+      
+    {myNutrition.map((item, index) => (
+    <NutritionComponent key = {index}
+                        calories = {item.calories}
+                        nutrients = {item.totalNutrients}/>
+    ))}
+    
     </div>
   );
 }
---
+++ src/NutritionComponent.js
@@ -0,0 +1,10 @@
+function NutritionComponent({calories, nutrients}) {
+    return(
+        <div>
+    <h2>{calories}</h2>
+    <p>{nutrients}</p>
+        </div>
+    )
+}
+
+export default NutritionComponent;
