806fec829bcbf50a26692fd2c3c0aef52e3492b1
mmarakesh <marinamakarova749@gmail.com>
2023-11-03 13:59:53 +0100

    change API request

--- src/App.js
+++ src/App.js
@@ -1,43 +1,69 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import NutritionComponent from './NutritionComponent';
+import LoaderPage from './Loader/LoaderPage';
 
 function App() {
-  const MY_ID = "996b8d70";
-  const MY_KEY = "13511e53a8922ecf13133c4451c2be24";
 
-  const [mySearch, setMySearch] = useState('');
-  const [myNutrition, setMyNutrition] = useState([]);
+  const [mySearch, setMySearch] = useState();
+  const [wordSubmit, setWordSubmit] = useState('');
+  const [stateLoader, setStateLoader] = useState(false)
+  
+  const MY_ID = "50520499";
+  const MY_KEY = "5de2e45a1138192adf18fc3d1b0c4b2c";
 
-  useEffect(() => {
-    getAnalysis();
-  }, [])
+  
 
-  const getAnalysis = async () => {
-    const response = await fetch(`https://api.edamam.com/api/nutrition-data?app_id=${MY_ID}&app_key=${MY_KEY}&nutrition-type=logging&ingr=avocado`)
-    const data = await response.json();
-    console.log(data)
-    setMyNutrition(data);
+  const getAnalysis = async (ingr) => {
+    setStateLoader(true);
+
+    const response = await fetch(`https://api.edamam.com/api/nutrition-details?app_id=${MY_ID}&app_key=${MY_KEY}`, 
+    {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ingr: ingr })
+    })
+
+    if(response.ok) {
+      setStateLoader(false);
+      const data = await response.json();
+      console.log(data)
+    } else {
+      setStateLoader(false);
+      alert('ingredients entered incorrectly');
+    }
   }
 
   const getData = (e) => {
   console.log(e.target.value);
   setMySearch(e.target.value);
   }
+
+  const finalSearch = (e) => {
+    e.preventDefault();
+    setWordSubmit(mySearch);
+  }
+
+  useEffect(() => {
+    if (wordSubmit !== '') {
+      let ingr = wordSubmit.split(/[,,;,\n,\r]/);
+      getAnalysis(ingr);
+    }
+  }, [wordSubmit])
+
   return (
     <div className="App">
+      {stateLoader && <LoaderPage/>} 
+
       <h1>Nutrinion Analysis</h1>
-      <form>
+      <form onSubmit={finalSearch}>
       <input onChange={getData} value={mySearch}></input>
+      <button>Click here</button>
       </form>
 
-      <button>Click here</button>
       
-    {myNutrition.map((item, index) => (
-    <NutritionComponent key = {index}
-                        calories = {item.calories}
-                        nutrients = {item.totalNutrients}/>
-    ))}
     
     </div>
   );
---
+++ src/Loader/Loader.js
@@ -0,0 +1,16 @@
+import './style.css';
+const Loader = () => {
+return (
+    <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+)
+}
+export default Loader;
